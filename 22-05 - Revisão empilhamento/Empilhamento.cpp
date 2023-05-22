7c82c8e51d3ed447b3692303ed64500e37131ffb
juninhooliani <70723220+juninhooliani@users.noreply.github.com>
2023-05-22 12:34:55 -0300

    Add files via upload

---
+++ 22-05 - Revisão empilhamento/Empilhamento.cpp
@@ -0,0 +1,52 @@
+using  namespace std;
+#include "teste.h"
+
+
+int main(){
+    Aluno *ini = NULL, *fim = NULL, *topo = NULL;
+
+    int op;
+    
+    do {
+        printf ("1 - Inserir | 2 - Listar Fila | 3 - Listar Pilha  | 4 - Excluir \n5 - Empilhar \n6 Limpar Fila | 7 - Limpar Pilha | 8 Limpar Tudo | 9 - Sair\n");
+        scanf ("%d", &op);
+        switch (op){
+        case 1:
+            inserir (&ini, &fim);
+        break;
+        case 2:
+            listarFila (&ini);
+        break;
+        case 3:
+            listarPilha (topo);
+        break;
+        case 4:
+            excluir (&ini, &fim);
+        break;
+        case 5:
+            limparPilha (&topo);
+            empilhar (&ini, &fim, &topo);
+        break;
+        case 6:
+            limparFila (&ini, &fim);
+        break;
+        case 7:
+            limparPilha (&topo);
+        break;
+        case 8:
+            limparFila (&ini, &fim);
+            limparPilha (&topo);
+        break;
+        case 9:
+            printf ("\nLimpando Elementos...\n");
+            limparFila (&ini, &fim);
+            limparPilha (&topo);
+            printf ("\nSaindo...\n");
+        break;
+        default:
+            printf ("Opcao invalida");
+        break;
+        }
+
+    }while (op != 9);
+}
---
+++ 22-05 - Revisão empilhamento/teste.h
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <conio.h>
+#include <ctype.h>
+#include <locale.h>
+
+namespace std {
+    typedef struct Aluno {
+        int ra;
+        Aluno *prox;
+    };
+
+    //funções
+
+    Aluno *novoElemento (){
+        Aluno *aux = (Aluno *)malloc (sizeof(Aluno));
+        if (aux == NULL) {
+            printf ("Sem espaco na memoria");
+            return NULL;
+        }
+        printf ("Digite o RA: ");
+        scanf ("%d", &aux->ra);
+        return aux;
+    }
+
+    void inserir (Aluno **ini, Aluno **f){
+        Aluno *aux = novoElemento ();
+        aux->prox=NULL;
+        if (*ini == NULL) {
+            *ini = aux;
+            *f = aux;
+        } else {
+            (*f)->prox = aux;
+            *f = aux;
+        }
+    }
+
+    Aluno *listarFila (Aluno **ini){
+        Aluno *aux = *ini;
+        if (aux == NULL) return NULL;
+        printf ("Ra: %d\n", aux->ra);
+        listarFila (&aux->prox);
+        return aux;
+    }
+
+    Aluno *listarPilha (Aluno *t){
+        Aluno *aux = t;
+        if (aux == NULL) return NULL;
+        else {
+            printf ("RA: %d\n", aux->ra);
+            listarPilha (aux->prox);
+        }
+        return aux;
+
+    }
+
+    
+
+    void excluir (Aluno **ini, Aluno **f){
+        if (*ini == NULL) printf ("Lista vazia");
+        else if (*ini == *f){
+            free (*ini);
+            *ini = NULL;
+            *f = NULL;
+        }
+        else {
+            Aluno *aux = *ini;
+            *ini = (*ini)->prox;
+            free (aux);
+        }
+    }
+
+    void limparFila (Aluno **ini, Aluno **f){
+        Aluno *aux = *ini;
+        while (aux != NULL){
+            *ini = (*ini)->prox;
+            free (aux);
+            aux = *ini;
+        }
+        *ini = NULL; *f = NULL;
+    }
+
+    void limparPilha (Aluno **t){
+       while (*t != NULL){
+        Aluno *aux = *t;
+        *t = (*t)->prox;
+        free (aux);
+       }
+    }
+
+
+    Aluno empilhar (Aluno **ini, Aluno **f, Aluno **t){
+        if (*ini == NULL) printf ("Fila vazia");
+        else {
+            Aluno *auxF = *ini;
+            Aluno *aux2, *aux3;
+            aux3 = (Aluno *) malloc (sizeof(Aluno));
+            aux3->prox=NULL;
+            aux2 = aux3;
+            while (auxF != NULL){
+                aux3->ra = auxF->ra;
+                aux3 = (Aluno *)malloc (sizeof(Aluno));
+                aux3->prox = aux2;
+                aux2 = aux3;
+                auxF = auxF->prox;
+            }
+            *t = aux3;
+        }
+    }
+
+}
